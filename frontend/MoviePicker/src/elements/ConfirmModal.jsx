368a4677ed35cc91dc12cd2ea5b22be9bcdb1e9c
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-09-25 23:25:30 +0530

    change done in cofirm modal of allignment and text soze

--- frontend/MoviePicker/src/elements/ConfirmModal.jsx
+++ frontend/MoviePicker/src/elements/ConfirmModal.jsx
@@ -2,10 +2,10 @@ import { RxCross2 } from "react-icons/rx";
 
 function ConfirmModal({ title, isClose, children }) {
     return (
-        <div className="w-full h-screen fixed inset-0 bg-black/50 flex justify-center items-center">
-            <div className="bg-gray-300/95 p-2.5 rounded-xl shadow-black shadow-2xl flex flex-col">
+        <div className="w-full h-screen fixed inset-0 bg-black/50 flex justify-center items-center ">
+            <div className="bg-gray-300/95 p-2.5 rounded-xl shadow-black shadow-2xl flex flex-col m-4">
                 <div className="flex flex-row justify-between mb-0.5">
-                    <div className="text-black font-medium text-lg mr-2.5">{title}</div>
+                    <div className="text-black font-medium text-lg md:text-2xl mr-2.5">{title}</div>
                     <div className="ml-2.5" onClick={isClose}>
                         <RxCross2 className='text-black hover:text-gray-400 hover:scale-95 font-extrabold text-2xl lg:text-3xl' />
                     </div>
